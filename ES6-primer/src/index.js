b2b53370a419d070478d0de1603955bda33a6570
TobeOfili <tobechukwu.ofili@pau.edu.ng>
2024-05-13 20:03:57 +0100

    ES6 primer

--- ES6-primer/src/index.js
+++ ES6-primer/src/index.js
@@ -1,8 +1,29 @@
+
 //console.log("Starting ES6 and above");
-import logger ,{appName as am,dummyfunction as df} from './tools.js'; 
+import logger,{appName, dummyFunction, weatherTag, Person, CustomMath, User, genericFunction11, Person1} from './tools.js'
 
-logger ("starting ES6 and above with modularity");
+logger("Starting ES6 and above with modularity");
 
-logger ("welcome! The application name is "+ am + ". There is a function that returns "+ df()) ;
-//notice the back tick before the welcome and at the end of the line
-logger (`welcome! The application name is "${am}". There is a function that returns "${df()}".`)
+
+logger("Welcome! The application name is " + appName + ". There is a function that returns " + dummyFunction() );
+
+logger(weatherTag`The temperature is ${25} degrees centigrade. The day is `);
+
+
+let person1 = new Person1("Pius", "Onobhayedo", "Male",1.7);
+let person2 = new Person1("Mary", "Joseph", "Female", undefined);
+
+
+person1.firstName = "peter"; //here we have deliberately used lowercase for first letter
+person2.firstName = "maria"; //here we have deliberately used lowercase for first letter
+
+
+
+logger(`Person 1 is ${person1.firstName} whose height is ${person1.height}. Person 2 is ${person2.firstName} whose height is ${person2.height}`);
+
+logger(CustomMath.sqrt(100));
+
+let user1 = new User("myusername","mypassword","Pius","Onobhayedo","Male",undefined); 
+logger(`The username of ${user1.firstName} is ${user1.username}`)
+
+logger(genericFunction11(11, 12, 15, 8));
